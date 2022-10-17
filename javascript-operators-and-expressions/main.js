0921a65b598b6c02b1adb36a5fd5d0ccdb1e27e3
Randy Gerada <randygerada@gmail.com>
2022-10-17 18:25:36 +0000

    Completed javascript operators and expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,54 @@
+var width = 10;
+var height = 5;
+
+var area = width * height;
+
+console.log('value of area: ', area);
+console.log('typeof area:', typeof area);
+
+var bill = 136;
+var payment = 155;
+
+var change = payment - bill;
+
+console.log('value of change: ', change);
+console.log('typeof change:', typeof change);
+
+var quizzes, midterm, final;
+
+quizzes = 10;
+midterm = 50;
+final = 70;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade: ', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName, lastName;
+
+firstName = 'Herman';
+lastName = 'Miller';
+
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH > 7;
+
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 1000;
+var isSparta = headCount === 300;
+
+console.log('value of isSpart : ', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+
+motto = fullName + '' + 'is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
