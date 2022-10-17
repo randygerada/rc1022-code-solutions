68213d19a346d087032abd6d46627c5cdf9b2080
Randy Gerada <randygerada@gmail.com>
2022-10-17 16:43:00 +0000

    javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName, isCool, totalPets;
+var uknown;
+var never;
+
+fullName = 'Randy Gerada';
+isCool = true;
+totalPets = 1;
+uknown = null;
+
+console.log('value of fullName: ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets: ', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+console.log('value of uknown: ', uknown);
+console.log('typeof uknown:', typeof uknown);
+console.log('value of never:', never);
+console.log('value of never:', typeof never);
