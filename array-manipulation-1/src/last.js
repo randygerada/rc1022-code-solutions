6bf5ed2e66bd287d59cc80169dafbbfcb5e9b96e
Randy Gerada <randygerada@gmail.com>
2022-10-21 16:26:31 +0000

    Incomplete.

--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -1,4 +1,7 @@
 /* exported last */
+// need to retrieve the last element in the array.
+// have to use a for loop in order to go through the whole array
+// use the length of the array as a condition.
 function last(array) {
   for (var i = 0; i < array.length; i++) {
     var getIndex = array.length - 1;
