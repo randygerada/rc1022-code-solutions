fc57a86b284ed9f25527e38ca234626741a53946
Randy Gerada <randygerada@gmail.com>
2022-10-24 17:07:55 +0000

    Completed dom querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+var el = document.querySelector('h1');
+console.log(el);
+
+console.dir(el);
+
+var getId = document.querySelector('#explanation');
+console.log(getId);
+console.dir(getId);
+
+var getClass = document.querySelector('.hint');
+console.log(getClass);
+console.dir(getClass);
+
+var forAllp = document.querySelectorAll('p');
+console.log(forAllp);
+
+var exampleLink = document.querySelectorAll('.example-link');
+console.log(exampleLink);
