168ca9f2bcf8878695948b69188bc6e75964383e
Randy Gerada <randygerada@gmail.com>
2022-10-25 17:08:46 +0000

    Completed dom manipulation.

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,19 +1,28 @@
 
 var clicks = 0;
-const updateClicks = document.getElementsByClassName('.click-count');
-updateClicks.textContent = 'this was clicked' + clicks + 'times';
-
-function numberOfClicks() {
-  clicks += 1;
-}
-
-// var $hotButton = document.querySelector('.hot-button');
+var $hotButton = document.querySelector('.hot-button');
 var $clickCount = document.querySelector('.click-count');
 
-$clickCount.addEventListener('click', numberOfClicks);
+$hotButton.addEventListener('click', handleHotButtonClick);
 
-/*
-const updateClicks = document.getElementsByClassName('.click-count').textContent;
-clicks += 1;
-document.getElementsByClassName('.click-count').textContent = updateClicks;
-*/
+function handleHotButtonClick(event) {
+  clicks += 1;
+  var temp;
+  if (clicks < 4) {
+    temp = 'cold';
+  } else if (clicks < 7) {
+    temp = 'cool';
+  } else if (clicks < 10) {
+    temp = 'tepid';
+  } else if (clicks < 13) {
+    temp = 'warm';
+  } else if (clicks < 16) {
+    temp = 'hot';
+  } else {
+    temp = 'nuclear';
+  }
+
+  $hotButton.className = 'hot-button ' + temp;
+
+  $clickCount.textContent = 'clicks: ' + clicks;
+}
