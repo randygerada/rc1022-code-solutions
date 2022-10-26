9d6aa2109279c15bb9b887485914ed6ae710a583
Randy Gerada <randygerada@gmail.com>
2022-10-26 05:31:51 +0000

    Refactored code.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,18 +1,19 @@
-var clicks = 0;
+var clicks = true;
 var $circle = document.querySelector('.circle');
 var background = document.querySelector('.backdrop');
 $circle.addEventListener('click', changeColorButton);
 
 function changeColorButton(event) {
   var changeBackgroundColor, changeColor;
-  if (clicks % 2 === 0) {
-    changeColor = 'dark';
-    changeBackgroundColor = 'background';
+  if (clicks) {
+    changeColor = 'circle dark';
+    changeBackgroundColor = 'backdrop night';
+    clicks = false;
   } else {
-    changeColor = 'circle';
-    changeBackgroundColor = 'backdrop';
+    changeColor = 'circle light';
+    changeBackgroundColor = 'backdrop day';
+    clicks = true;
   }
-  clicks++;
 
   $circle.className = changeColor;
   background.className = changeBackgroundColor;
