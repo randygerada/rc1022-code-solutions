9bf7fda97d6d082ac210e706d0d9bb3fe15e451d
Randy Gerada <randygerada@gmail.com>
2022-10-28 21:21:51 +0000

    Refactored code.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -3,9 +3,11 @@
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 
+/* check for null */
 if (previousTodosJSON !== null) {
   todos = JSON.parse(previousTodosJSON);
 }
+
 window.addEventListener('beforeunload', event => {
   event.preventDefault();
   var todosJSON = JSON.stringify(todos);
