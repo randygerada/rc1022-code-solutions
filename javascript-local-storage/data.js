db829f5f756a632c70c0a3f69d10e0afa81bceca
Randy Gerada <randygerada@gmail.com>
2022-10-28 21:19:54 +0000

    Completed javascript-local-storage.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -3,10 +3,11 @@
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
 window.addEventListener('beforeunload', event => {
   event.preventDefault();
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
 });
-
-todos = JSON.parse(previousTodosJSON);
