28a961717f6176ef6b274cee354c63f4e0b75ebb
Randy Gerada <randygerada@gmail.com>
2022-10-28 21:37:57 +0000

    Completed javascript-modal.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -2,6 +2,10 @@ var modal = document.querySelector('.modal');
 var blueBtn = document.querySelector('.blue-btn');
 var redBtn = document.querySelector('.red-btn');
 
+blueBtn.addEventListener('click', toggleModal);
+redBtn.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
+
 function toggleModal() {
   modal.classList.toggle('show-modal');
 }
@@ -11,7 +15,3 @@ function windowOnClick(event) {
     toggleModal();
   }
 }
-
-blueBtn.addEventListener('click', toggleModal);
-redBtn.addEventListener('click', toggleModal);
-window.addEventListener('click', windowOnClick);
