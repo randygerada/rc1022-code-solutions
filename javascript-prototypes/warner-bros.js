fc1c164156ab87e3fcbe2c4818d0e63b4eb6dccd
Randy Gerada <randygerada@gmail.com>
2022-11-07 22:27:14 +0000

    Completed JavaScript Protoypes Exercise.

---
+++ javascript-prototypes/warner-bros.js
@@ -0,0 +1,33 @@
+/* exported yakko, wakko, dot */
+
+var yakko = {
+  age: 14,
+  name: 'Yakko',
+  role: 'brother'
+};
+
+var wakko = {
+  age: 11,
+  name: 'Wakko',
+  role: 'brother'
+};
+
+var dot = {
+  age: 10,
+  name: 'Dot',
+  role: 'sister'
+};
+
+var warnerProtoType = {
+  describe: function () {
+    var description = this.name + ' is a ' +
+      this.age + '-year-old Warner ' + this.role + '.';
+    return description;
+  }
+};
+
+Object.setPrototypeOf(yakko, warnerProtoType);
+
+Object.setPrototypeOf(wakko, warnerProtoType);
+
+Object.setPrototypeOf(dot, warnerProtoType);
