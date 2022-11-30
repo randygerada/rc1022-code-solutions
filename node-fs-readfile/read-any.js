d96449d024ad1b1e6856f0357e349c72f78a1970
Randy Gerada <randygerada@gmail.com>
2022-11-30 22:55:32 +0000

    Refactored code on read-any.js file.

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,24 +1,11 @@
 const fs = require('fs');
 
-const argv = process.argv;
-const argvs = argv.slice(2);
-const txtFile = argvs[0];
+const filename = process.argv[2];
 
-if (txtFile === 'cunningham.txt') {
-  fs.readFile('cunningham.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-} else if (txtFile === 'hopper.txt') {
-  fs.readFile('hopper.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-} else if (txtFile === 'dijkstra.txt') {
-  fs.readFile('dijkstra.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-} else {
-  console.log('invalid entry');
-}
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(data);
+});
