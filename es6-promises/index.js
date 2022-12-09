114a00a20453d44d92b9711c2c46bb034dd61a96
Randy Gerada <randygerada@gmail.com>
2022-12-09 18:38:02 +0000

    Completed es6 promises exercise.

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -2,10 +2,8 @@ const takeAChance = require('./take-a-chance');
 
 const newObj = takeAChance('Randy');
 
-newObj.then((value) => {
-  console.log(value);
-});
-
-newObj.catch((error) => {
-  console.log(error);
+newObj.then((message) => {
+  console.log(message);
+}).catch((message) => {
+  console.error(message);
 });
