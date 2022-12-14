4133de247334f5628f81ee70c6b08c995f7e921b
Randy Gerada <randygerada@gmail.com>
2022-12-14 21:35:18 +0000

    Completed react class components exercise.

--- react-class-components/src/index.jsx
+++ react-class-components/src/index.jsx
@@ -24,25 +24,3 @@ const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
 root.render(element);
-/*
-
-function CustomButton(props) {
-  return (
-    <button>{props.text}</button>
-  );
-}
-
-const element = (
-  <div>
-    <CustomButton text="I" />
-    <CustomButton text="know" />
-    <CustomButton text="React!" />
-  </div>
-);
-
-const container = document.querySelector('#root');
-const root = ReactDOM.createRoot(container);
-
-root.render(element);
-
-*/
