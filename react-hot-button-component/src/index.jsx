06055b29697c7dc6e09b4a303a2a2e876df0ef7b
Randy Gerada <randygerada@gmail.com>
2022-12-19 18:57:42 +0000

    Completed react hot button component challenge.

--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -1,33 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-class HotButton extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicks: 0,
-    };
-
-    this.colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo'];
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(state => ({
-      clicks: state.clicks + 1,
-    }));
-  }
-
-  render() {
-    const colorIndex = Math.floor(this.state.clicks / 3) % this.colors.length;
-    const color = this.colors[colorIndex];
-
-    return (
-      <button style={{ backgroundColor: color }} onClick={this.handleClick}>
-        Hot Button
-      </button>
-    );
-  }
-}
+import { CustomButton } from './hot-button';
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<CustomButton />);
